1e437e6d7dcfa8f050ceedbd4ce81423a61750f7
FabioCostantino84 <costantinofabio84@gmail.com>
2023-10-03 15:36:36 +0200

    add HeaderTop

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -2,8 +2,8 @@
 export default {
     name: 'HeaderTop',
     data() {
-        return{
-            
+        return {
+
         }
     }
 
@@ -11,12 +11,31 @@ export default {
 </script>
         
 <template>
-    
 
+    <div id="header_top">
+        <div class="container w-75">
+            <div class="row p-3">
+                <div class="col-6"><i class="fa-solid fa-clock"></i> Open Hours: Mon - Sat - 9:00 - 18:00 </div>
+                <div class="col-6 d-flex justify-content-around"> 
+                    <div><i class="fa-solid fa-phone"></i> +1 (305) 1234-5678</div>
+                    <div><i class="fa-solid fa-envelope"></i> hello@exeple.com</div>
+                    <div><i class="fa-brands fa-facebook-f"></i></div>
+                    <div><i class="fa-brands fa-twitter"></i></div>
+                    <div><i class="fa-brands fa-linkedin-in"></i></div>
+                </div>
+            </div>
+        </div>
 
+    </div>
 </template>
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
 
+#header_top, .card {
+    background-color: $SB_blu;
+    color: $SB_grigioscuro;
+    border: 0;
+}
 </style>
