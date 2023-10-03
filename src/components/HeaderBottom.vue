c2144b311036db2386ea69a3ad7bec578ff48471
FabioCostantino84 <costantinofabio84@gmail.com>
2023-10-03 16:53:23 +0200

    add nav bar and hover

--- src/components/HeaderBottom.vue
+++ src/components/HeaderBottom.vue
@@ -2,8 +2,8 @@
 export default {
     name: 'HeaderBottom',
     data() {
-        return{
-            
+        return {
+
         }
     }
 
@@ -11,26 +11,45 @@ export default {
 </script>
         
 <template>
-
-    <div class="container">
+    <div class="container bg-transparent">
         <div class="row">
             <div class="col-4">
                 <img src="../assets/svg/svg-7.svg" class="" alt="">
             </div>
-            <div class="col-8"></div>
+            <div class="col-8">
+                <nav class="nav d-flex justify-content-around align-items-center p-3">
+                    <a class="nav-link" href="#">HOME</a>
+                    <a class="nav-link" href="#">SERVICES</a>
+                    <a class="nav-link" href="#">ABOUT</a>
+                    <a class="nav-link" href="#">PROJECTS</a>
+                    <a class="nav-link" href="#">RESULTS</a>
+                    <a class="nav-link" href="#">GET IN TOUCH</a>
+                </nav>
+            </div>
         </div>
     </div>
-    
-
-
 </template>
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
 
-img{
+img {
     padding: 15px 40px 15px 15px;
     width: 50%;
 }
 
+a {
+    color: $SB_grigioscuro;
+    font-size: large;
+    font-weight: 500;
+    padding: 10px 20px;
+
+}
+
+a:hover {
+    background-color: $SB_verdeacqua_1;
+    color: $SB_grigiochiaro;
+    border-radius: 5px;
+}
 </style>
