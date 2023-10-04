db79f28a47f6114b778b36807f23125bb7b4c59e
FabioCostantino84 <costantinofabio84@gmail.com>
2023-10-04 16:34:32 +0200

    add font family in Appvue and index

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,14 @@ import AppFooter from './components/AppFooter.vue'
 </template>
 
 
-<style lang="scss" scoped>
+<style lang="scss">
+// chiedere come farlo funzionare
+/* @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'); */
+
+body{
+  font-family: 'Poppins', sans-serif;
+}
+
+
 
 </style>
