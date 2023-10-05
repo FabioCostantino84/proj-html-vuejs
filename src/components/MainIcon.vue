f5a84b8038153d6e76158eab08e1bdbbb1934723
FabioCostantino84 <costantinofabio84@gmail.com>
2023-10-05 16:21:20 +0200

    add final MainIcon

--- src/components/MainIcon.vue
+++ src/components/MainIcon.vue
@@ -8,3 +8,37 @@ export default {
     },
 }
 </script>
+
+<template>
+    <div id="icon">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex align-items-center justify-content-around">
+                    <img style="width: 95px;" src="../assets/img/logo-4.png" alt="">
+                    <img style="width: 60px;" src="../assets/img/logo-5.png" alt="">
+                    <img style="width: 140px;" src="../assets/img/logo-1.png" alt="">
+                    <img style="width: 130px;" src="../assets/img/logo-2.png" alt="">
+                    <img style="width: 100px;" src="../assets/img/logo-3.png" alt="">
+                    <img style="width: 95px;" src="../assets/img/logo-4.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+#icon {
+    background-color: $SB_blu;
+    height: 290px;
+}
+
+.container {
+    width: 60%;
+}
+
+img {
+    width: 90px;
+}
+</style>
