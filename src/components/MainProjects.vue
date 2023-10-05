b63e9163de4d345f888a9eea27793a1dbcbd541c
FabioCostantino84 <costantinofabio84@gmail.com>
2023-10-05 12:59:16 +0200

    add final MainProjects

--- src/components/MainProjects.vue
+++ src/components/MainProjects.vue
@@ -30,7 +30,8 @@ export default {
                     <a class="nav-link" href="#">INSTITUTIONAL</a>
                     <a class="nav-link" href="#">SOCIAL</a>
                     <a class="nav-link" href="#">EVENTS</a>
-                    <a class="nav-link" href="#">INNOVATIONS</a>
+                    <a class="nav-link" href="#">INNOVATION</a>
+                    <a class="nav-link" href="#">ENVIRONMENT</a>
                     <a class="nav-link" href="#">TECHNOLOGY</a>
                 </nav>
             </div>
@@ -40,7 +41,52 @@ export default {
                 <div class="card border-0 py-4">
                     <img src="../assets/img/project-1.jpg" class="card-img" alt="...">
                     <div class="card-img-overlay text-center">
-                        <h5 class="card-title">Academic professional program in social media</h5>
+                        <h5 class="card-title px-4">Academic professional program in social media</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card border-0 py-4">
+                    <img src="../assets/img/project-2.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay text-center">
+                        <h5 class="card-title px-5">President's speech at the annual meeting</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card border-0 py-4">
+                    <img src="../assets/img/project-3.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay text-center">
+                        <h5 class="card-title px-5">International business trip in Shanghai</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card border-0 py-4">
+                    <img src="../assets/img/project-4.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay text-center">
+                        <h5 class="card-title px-5">Technology workshop with education theme</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card border-0 py-4">
+                    <img src="../assets/img/project-5.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay text-center">
+                        <h5 class="card-title px-4">Donation of clothes and food to the partner NGO</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card border-0 py-4">
+                    <img src="../assets/img/project-6.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay text-center">
+                        <h5 class="card-title px-4">Confraternization of the procurement team</h5>
                     </div>
                 </div>
             </div>
@@ -77,7 +123,7 @@ a {
     color: $SB_grigioscuro;
     font-size: large;
     font-weight: 500;
-    padding: 10px 20px;
+    padding: 11px 15px;
 
 }
 
@@ -103,5 +149,9 @@ h5{
     font-weight: 600;
     font-size: 25px;
     padding-top: 305px;
+}
+
+.row{
+    padding-bottom: 150px;
 }
 </style>
